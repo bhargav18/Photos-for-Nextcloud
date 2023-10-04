8eb9552c1a200fc4ba0910e1160b1a22cf4ba39b
Bhargav Donga <dongabd@amazon.com>
2023-10-04 14:36:10 -0700

    Initial Commit

---
+++ fireworks/fireworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  fireworksApp.swift
+//  fireworks
+//
+//  Created by Donga, Bhargav on 10/4/23.
+//
+
+import SwiftUI
+
+@main
+struct fireworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ fireworksUITests/fireworksUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  fireworksUITestsLaunchTests.swift
+//  fireworksUITests
+//
+//  Created by Donga, Bhargav on 10/4/23.
+//
+
+import XCTest
+
+final class fireworksUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
